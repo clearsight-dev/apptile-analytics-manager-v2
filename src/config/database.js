7009be801f9eab52b8b966630a8f62faa8871bfe
mansin2001 <156772234+mansin2001@users.noreply.github.com>
2024-08-14 13:13:26 +0530

    fixed env keys name

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    username: process.env.AWM_ANALYTICS_DB_USER || "user",
-    password: process.env.AWM_ANALYTICS_DB_PWD || "password",
-    database: process.env.AWM_ANALYTICS_DB_NAME || "apptile-analytics-manager",
-    host: process.env.AWM_ANALYTICS_DB_HOST || "host",
+    username: process.env.DB_USER || "user",
+    password: process.env.DB_PWD || "password",
+    database: process.env.DB_NAME || "apptile-analytics-manager",
+    host: process.env.DB_HOST || "host",
     port: "5432",
     dialect: "postgres",
     dialectOptions: {
@@ -13,10 +13,10 @@ module.exports = {
     },
   },
   production: {
-    username: process.env.AWM_ANALYTICS_DB_USER || "user",
-    password: process.env.AWM_ANALYTICS_DB_PWD || "password",
-    database: process.env.AWM_ANALYTICS_DB_NAME || "apptile-analytics-manager",
-    host: process.env.AWM_ANALYTICS_DB_HOST || "host",
+    username: process.env.DB_USER || "user",
+    password: process.env.DB_PWD || "password",
+    database: process.env.DB_NAME || "apptile-analytics-manager",
+    host: process.env.DB_HOST || "host",
     port: "5432",
     dialect: "postgres",
     dialectOptions: {
--- src/helpers/sequalize.ts
+++ src/helpers/sequalize.ts
@@ -1,11 +1,11 @@
 import { Sequelize } from "sequelize";
 
 export const sequelize = new Sequelize(
-  process.env.AWM_ANALYTICS_DB_NAME || "apptile-analytics-manager",
-  process.env.AWM_ANALYTICS_DB_USER || "user",
-  process.env.AWM_ANALYTICS_DB_PWD || "password",
+  process.env.DB_NAME || "apptile-analytics-manager",
+  process.env.DB_USER || "user",
+  process.env.DB_PWD || "password",
   {
-    host: process.env.AWM_ANALYTICS_DB_HOST || "host",
+    host: process.env.DB_HOST || "host",
     dialect: "postgres",
     logging: false,
   }
